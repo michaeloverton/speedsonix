19188c40e21600a0e24b0ef49af130dc19b9cb2b
Michael Brown <m.overtonbrown@gmail.com>
2023-02-26 23:53:00 -0800

    add music and tweak scene

--- Assets/Scripts/Objects/BouncePad.cs
+++ Assets/Scripts/Objects/BouncePad.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class BouncePad : MonoBehaviour
 {
     public float bounceForce = 200f;
+    float cooldownTime = 1f;
+    bool cooldown = false;
 
     void OnCollisionEnter(Collision collision) {
-        if(collision.rigidbody != null)
-            collision.rigidbody.AddForce(collision.gameObject.transform.up * bounceForce, ForceMode.Impulse);
+        if(collision.rigidbody != null && !cooldown) {
+            collision.rigidbody.velocity = Vector3.zero;
+            collision.rigidbody.AddForce(transform.up * bounceForce, ForceMode.Impulse);
+            cooldown = true;
+            Invoke("resetCooldown", cooldownTime);
+        }
+    }
+
+    void resetCooldown()
+    {
+        cooldown = false;
     }
 }
